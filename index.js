91af93a12503ad2755a02374b7e2f7c7b750cdb3
José Hidalgo <josehidalgo990@gmail.com>
2020-11-26 23:47:28 -0400

    localStorage funcionando

--- index.js
+++ index.js
@@ -1,3 +1,34 @@
+function sabe() {
+  if (localStorage.length > 1) {
+    document.querySelector("h3").style.display = "none";
+  }
+  var indexStorage = Number(localStorage[0]);
+  for (i = 1; i < localStorage.length; i++) {
+    if (localStorage.getItem(indexStorage)) {
+      var toDoContainer = document.querySelector("#toDoContainer");
+
+      var toDoShell = document.createElement("div");
+      toDoShell.classList.add("toDoShell");
+
+      toDoContainer.appendChild(toDoShell);
+
+      var toDoText = document.createElement("span");
+
+      toDoShell.appendChild(toDoText);
+      toDoText.innerHTML = localStorage.getItem(indexStorage);
+      toDoText.id = indexStorage;
+      indexStorage--;
+    } else {
+      indexStorage--;
+      i--;
+    }
+  }
+}
+window.onload = function () {
+  sabe();
+};
+
+///////
 var toDoItems = [];
 
 function ToDo(description) {
@@ -5,18 +36,21 @@ function ToDo(description) {
   this.complete = false;
 }
 
+var indexStorage = 0;
 ToDo.prototype.completeToDo = function () {
   this.complete = true;
 };
-
 function buildToDo(todo, index) {
   var toDoShell = document.createElement("div");
   toDoShell.classList.add("toDoShell");
   var toDoText = document.createElement("span");
+  indexStorage = Number(localStorage[0]);
+  indexStorage++;
   toDoText.innerHTML = todo.description;
-  toDoText.id = index;
+  toDoText.id = indexStorage;
+  localStorage.setItem(indexStorage, todo.description);
+  localStorage.setItem("0", indexStorage);
   if (todo.complete) {
-    // toDoText.className = "completeText";
     toDoText.classList.add("completeText");
   }
   toDoShell.appendChild(toDoText);
@@ -43,7 +77,7 @@ function addToDo() {
   var input = document.querySelector("#toDoInput");
   var todo = new ToDo(input.value);
 
-  toDoItems.push(todo);
+  toDoItems.unshift(todo);
   input.value = "";
   document.querySelector("h3").style.display = "none";
   displayToDos();
@@ -53,11 +87,17 @@ var button = document.querySelector("#addButton");
 var input = document.querySelector("#toDoInput");
 
 button.addEventListener("click", () => {
-  if (input.value !== "") addToDo();
+  if (input.value !== "") {
+    addToDo();
+    location.reload();
+  }
 });
 
 input.addEventListener("keyup", function (event) {
-  if (event.keyCode === 13 && input.value !== "") addToDo();
+  if (event.keyCode === 13 && input.value !== "") {
+    addToDo();
+    location.reload();
+  }
 });
 
 function completeToDo(event) {
